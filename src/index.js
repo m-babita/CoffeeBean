4b06bf66941dc8b10957b5fb85d9b7c98ec5056e
m-babita <babita.majumdar.1@gmail.com>
2021-09-22 00:43:52 +0530

    only contacts remaining

--- src/index.js
+++ src/index.js
@@ -4,14 +4,27 @@ const navMenu = document.getElementById('nav-menu'),
    closeMenu = document.getElementById('nav-close')
 
 //    show menu
+if(toggleMenu){
 toggleMenu.addEventListener('click', ()=>{
     navMenu.classList.toggle('show')
 })
+}
 
 //    hide menu
+if(closeMenu){
 closeMenu.addEventListener('click', ()=>{
     navMenu.classList.remove('show')
 })
+}
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*===== MOUSEMOVE HOME IMG =====*/
 document.addEventListener('mousemove', move);
@@ -26,13 +39,59 @@ function move(e){
     })
 }
 
+/*==================== SWIPER DISCOVER ====================*/
+let swiper = new Swiper(".discover__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 32,
+    coverflowEffect: {
+        rotate: 0,
+    },
+})
+
+/*==================== VIDEO ====================*/
+const videoFile = document.getElementById('video-file'),
+      videoButton = document.getElementById('video-button'),
+      videoIcon = document.getElementById('video-icon')
+
+function playPause(){ 
+    if (videoFile.paused){
+        // Play video
+        videoFile.play()
+        // We change the icon
+        videoIcon.classList.add('bx-pause')
+        videoIcon.classList.remove('bx-play')
+    }
+    else {
+        // Pause video
+        videoFile.pause(); 
+        // We change the icon
+        videoIcon.classList.remove('bx-pause')
+        videoIcon.classList.add('bx-play')
+
+    }
+}
+videoButton.addEventListener('click', playPause)
+
+function finalVideo(){
+    // Video ends, icon change
+    videoIcon.classList.remove('bx-pause')
+    videoIcon.classList.add('bx-play')
+}
+// ended, when the video ends
+videoFile.addEventListener('ended', finalVideo)
+
+
 /*===== GSAP ANIMATION =====*/
 // NAV
-gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10})
-gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
+gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:1, y: 10})
+gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 1.2, y: 30, stagger: 0.2,})
 
 // HOME
-gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
-gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
-gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
-gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
+gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.2, y: 30})
+gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.4, y: 30})
+gsap.from('.home__button', {opacity: 0, duration: 1, delay:1.6, y: 30})
+gsap.from('.home__img', {opacity: 0, duration: 1, delay:1, y: 30})
